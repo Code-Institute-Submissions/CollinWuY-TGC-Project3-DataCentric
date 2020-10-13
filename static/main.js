1de1c56f1bffb81c45c271f384eb119b242e36f6
CollinWuY <collinwu.y@gmail.com>
2020-10-13 07:33:49 +0000

    Search bar and Search Filter Complete

---
+++ static/main.js
@@ -0,0 +1,52 @@
+let bookList = [
+    {"name": "Dungeon and Dragons"},
+    {"name": "Call of Cthulhu"},
+    {"name": "Blades in the Dark"},
+    {"name": "Warhammer Fantasy"},
+    {"name": "Deadlands"},
+    {"name": "Cyberpunk"},
+    {"name": "Paranoia"},
+    {"name": "Shadow of the Demon Lord"},
+    {"name": "Legend of the Five Rings"},
+    {"name": "Star Wars Roleplaying"},
+    {"name": "Starfinder"},
+    {"name": "Fate Core"},
+    {"name": "Star Trek Adventures"},
+    {"name": "Bubblegumshoe"},
+    {"name": "Feng Shui"},
+    {"name": "Masks"},
+    {"name": "Fiasco"},
+    {"name": "Numenera"},
+]
+
+buildList(bookList)
+
+function buildList(data){
+    let list = document.getElementById('menu');
+    list.innerHTML = ""
+    for(let i = 0; i < data.length; i++){
+        let newRow = `<li>${data[i].name}</li>`;
+        list.innerHTML += newRow
+    }
+}
+
+$('#search').on('keyup', function(){
+    let value = $(this).val();
+    // console.log("Value:", value); // Check value on typing in search
+    let data = searchList(value, bookList);
+    buildList(data);
+})
+
+function searchList(value, data){
+    let filteredData = [];
+
+    for(let i = 0; i < data.length; i++){
+        value = value.toLowerCase();
+        let name = data[i].name.toLowerCase();
+
+        if (name.includes(value)){
+            filteredData.push(data[i])
+        }
+    }
+    return filteredData
+}

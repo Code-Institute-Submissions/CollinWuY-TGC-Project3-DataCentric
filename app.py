6720e6cec7eb19789902783ec5f4c49bd25a54be
CollinWuY <collinwu.y@gmail.com>
2020-11-06 22:22:24 +0000

    Create new book review is linked to MongoDB

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 from dotenv import load_dotenv
 from bson.objectid import ObjectId
 from bson.json_util import dumps, loads
@@ -16,12 +16,13 @@ client = pymongo.MongoClient(MONGO_URL)
 db = client[DB_NAME]
 
 app = Flask(__name__)
+app.secret_key = os.environ.get('SECRET_KEY')
 
 
 @app.route('/')
 def homepage():
     books = list(db.books.find().sort("reviews", -1))
-    top4 = books[:4]
+    top5 = books[:5]
     
     search_list = list()
     items_set = set()
@@ -31,7 +32,7 @@ def homepage():
             items_set.add(book['category'])
             search_list.append(book)
     # print(search_list)
-    return render_template('home.template.html', books=books, search_list=search_list, top4=top4)
+    return render_template('home.template.html', books=books, search_list=search_list, top5=top5)
 
 
 @app.route('/livesearch', methods=["POST", "GET"])
@@ -51,7 +52,39 @@ def show_create_form():
 
 @app.route('/create', methods=["POST"])
 def process_create_form():
+    name = request.form.get('bookName')
+    category = request.form.get('bookCategory')
+    author = request.form.get('bookAuthor')
+    release_date = request.form.get('bookRelease_Date')
+    price = request.form.get('bookPrice')
+    comments = request.form.get('bookComments')
+    image = request.form.get('bookImage')
+    reviews = request.form.getlist('rate')
+    reviews = int(reviews[0])
+    
+    new_record = {
+        "category": category,
+        "name": name,
+        "author": author,
+        "release_date": release_date,
+        "price": price,
+        "reviews": reviews,
+        "comments": comments,
+        "image": image
+    }
+
+    print(new_record)
+    db.books.insert_one(new_record)
+    flash("New book review is created successfully!", "success")
+
     return redirect(url_for('homepage'))
+
+@app.route('/edit/<book_id>')
+def show_edit_book(book_id):
+    book = db.books.find_one({
+        '_id': ObjectId(book_id)
+    })
+    return render_template('edit_book.template.html', book=book) 
 
 # def query():
 #     try:

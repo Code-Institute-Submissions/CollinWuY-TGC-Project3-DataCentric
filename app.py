c2b96218726da7f6a98311d0735ff42d061690b9
CollinWuY <collinwu.y@gmail.com>
2020-11-18 06:23:01 +0000

    Setting Flask Debug Mode to False

--- app.py
+++ app.py
@@ -388,8 +388,7 @@ def confirm_delete_book(book_id):
     return redirect(url_for("homepage"))
 
 
-# "magic code" -- boilerplate
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
-            debug=True)
+            debug=False)
